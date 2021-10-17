ee0c186bb335bab84c1b846fd6b272ba07aa7562
Oguzhan Ince <oguzhan824@gmail.com>
2021-10-17 20:26:33 +0300

    Create edit password feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   root to: 'main#index'
 
+  get "password", to: 'passwords#edit', as: :edit_password
+  patch "password", to: 'passwords#update'
+
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
 
