a57dbda5f5138d061b7f52048d1c9197dd1f0121
Oguzhan İnce <oguzhan824@gmail.com>
2021-10-17 03:29:25 +0300

    Create sign in feature (#5)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
 
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
-  delete "logout", to: 'sessions#destroy'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
 
   get 'about', to: 'about#index'
 end
