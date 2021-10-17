065ab57e2ecc741df07d96a073fd9f55ea616e14
Oguzhan Ince <oguzhan824@gmail.com>
2021-10-17 21:06:45 +0300

    Create forgot password feature

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,11 @@ Rails.application.routes.draw do
 
   delete 'logout', to: 'sessions#destroy'
 
+  get "password/reset", to:'password_resets#new'
+  post "password/reset", to:'password_resets#create'
+
+  get "password/reset/edit", to:'password_resets#edit'
+  patch "password/reset/edit", to:'password_resets#update'
+
   get 'about', to: 'about#index'
 end
