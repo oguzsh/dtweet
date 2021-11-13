0aa98af5c00b462f2a65313c9ce7a7be85fd0d1a
Oguzhan Ince <oguzhan824@gmail.com>
2021-11-14 02:38:57 +0300

    Tweet model

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  resources :tweets
+
   get 'about', to: 'about#index'
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_06_154102) do
+ActiveRecord::Schema.define(version: 2021_11_12_201852) do
 
   create_table "sessions", force: :cascade do |t|
     t.string "session_id", null: false
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2021_11_06_154102) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "body"
+    t.datetime "publish_at"
+    t.string "tweet_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,8 +43,11 @@ ActiveRecord::Schema.define(version: 2021_11_06_154102) do
     t.string "uid"
     t.string "name"
     t.string "username"
+    t.string "token"
+    t.string "secret_key"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
