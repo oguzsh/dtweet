23d132c969aad563c96025e50603b59e49e66257
Oguzhan Ince <oguzhan824@gmail.com>
2021-11-14 02:36:38 +0300

    helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  def user_signed_in
+    redirect_to root_path, alert: 'You need to connect your twitter account!' unless user_signed_in?
+  end
 
+  def after_sign_in_path_for(_resource)
+    tweets_path
+  end
 end
