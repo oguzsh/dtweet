98229afcefa7a8a052cad230c2779bbb72c76c40
Oguzhan Ince <oguzhan824@gmail.com>
2021-11-14 02:42:05 +0300

    Twitter Client method created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :tweets
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[twitter]
@@ -11,6 +11,17 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
       user.username = auth.info.nickname
+      user.token = auth.credentials.token
+      user.secret_key = auth.credentials.secret
+    end
+  end
+
+  def twitter_client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        =  Rails.application.credentials.fetch(:twitter_api_public_key)
+      config.consumer_secret     =  Rails.application.credentials.fetch(:twitter_api_secret_key)
+      config.access_token        =  token
+      config.access_token_secret =  secret_key
     end
   end
 
