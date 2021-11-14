7319c9c04f51e1fe8641831cb95b8383a6f7ed4c
Oguzhan Ince <oguzhan824@gmail.com>
2021-11-14 19:33:54 +0300

    Init Sidekiq and removed about page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -75,4 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.active_job.queue_adapter = :sidekiq
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  if Rails.env.development?
+    require 'sidekiq/web'
+
+    # Sidekiq
+    mount Sidekiq::Web, at: '/sidekiq'
+  end
+
   root to: 'home#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :tweets
-
-  get 'about', to: 'about#index'
 end
-
