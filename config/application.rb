cebfad87fc32562c895079862d14828706bfb12e
Oguzhan Ince <oguzhan824@gmail.com>
2021-11-15 00:32:33 +0300

    Timezone

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -16,7 +16,8 @@ module ScheduledTweets
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_attributes = false
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
