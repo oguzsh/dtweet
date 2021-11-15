ab5e6b990d98d627e17c38cbff18e8fd79818c78
Oguzhan Ince <oguzhan824@gmail.com>
2021-11-15 23:15:05 +0300

    Timezone fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_timezone
+
+  def set_timezone
+    Time.zone = request.cookies['browser_timezone']
+  end
+
   def user_signed_in
     redirect_to root_path, alert: 'You need to connect your twitter account!' unless user_signed_in?
   end
--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ module ScheduledTweets
     # in config/environments, which are processed later.
     #
 
-    config.time_zone = 'Central Time (US & Canada)'
     config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
   end
