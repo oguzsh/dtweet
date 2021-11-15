93013b201b9374887e19f454854f18d433cf7140
Oguzhan Ince <oguzhan824@gmail.com>
2021-11-15 11:24:29 +0300

    Timezone fix

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,9 @@ module ScheduledTweets
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+
+    config.time_zone = 'Central Time (US & Canada)'
     config.active_record.default_timezone = :local
-    config.active_record.time_zone_aware_attributes = false
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
