0f7d7d0957cd39385f005c8ae386f14963544c26
Oguzhan Ince <oguzhan824@gmail.com>
2021-11-20 17:08:38 +0300

    Init Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    theme: {
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+        },
+        fontFamily: {
+            'primary': ['DM Sans', 'sans-serif'],
+            'secondary': ['Mulish', 'sans-serif'],
+        },
+        borderWidth: {
+            default: '1px',
+            '0': '0',
+            '2': '2px',
+            '4': '4px',
+        },
+        extend: {
+            colors: {
+                black: '#19191b',
+                grey: '#696871',
+                lightblue: '#26AAe1',
+            },
+            spacing: {}
+        }
+    }
+}
