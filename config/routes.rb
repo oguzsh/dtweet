e8dbbca359ce0dde5559a2c363e7c8792608d903
Oguzhan Ince <oguzhan824@gmail.com>
2021-11-26 15:30:08 +0300

    auth system added for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  if Rails.env.development?
-    require 'sidekiq/web'
+  require 'sidekiq/web'
 
-    # Sidekiq
+  scope :monitoring do
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(Rails.application.credentials[:sidekiq][:username])) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(Rails.application.credentials[:sidekiq][:password]))
+    end if Rails.env.production?
+
     mount Sidekiq::Web, at: '/sidekiq'
   end
+
 
   root to: 'home#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
